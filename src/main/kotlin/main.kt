72734e2bc1c32eaf6e077a34136892f1943c31eb
Daniel James <danieldragonhatcher@gmail.com>
2021-09-01 09:54:28 -0700

    use relative path

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,7 @@ import dev.dewy.nbt.NbtReader
 import java.io.File
 
 fun main() {
-    val litematic = File("C:\\Users\\danie\\IdeaProjects\\AmethystBuilder\\src\\main\\resources\\Simple Cluster Example.litematic")
+    val litematic = File("src\\main\\resources\\Simple Cluster Example.litematic")
     val nbt = NbtReader.fromFile(litematic)
     println(readNbtIntoCluster(nbt))
 }
